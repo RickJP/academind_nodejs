8339f676e26c38bfa20f4c2704dbc595b03f7c9e
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-27 14:48:18 +0900

    - Added inline styles temporarily to make it look better

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(shopRoutes);
 
 // Catch all route
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not Found</h1>')
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
