2189c192c320e0e0835a4df1c143a51fc9511b08
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-27 18:04:41 +0900

    Installed templating engines: ejs, pug & handlebars

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const bodyParser = require('body-parser');
 const path = require('path');
@@ -11,7 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 // Catch all route
