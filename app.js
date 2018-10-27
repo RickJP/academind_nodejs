2c323a89216c4e2916b83e8beae6e27860c0af43
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-27 20:23:57 +0900

    Marked up the pug files for 404.html & add-product, added code to render them

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -16,7 +18,8 @@ app.use(shopRoutes);
 
 // Catch all route
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', {docTitle: 'Pge Not Found'});
+  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
