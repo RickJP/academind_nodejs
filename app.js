80a4f7f108cddf9b9afa83c6691c45ba16ab6836
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-27 21:03:30 +0900

    Used templating to avoid markup duplication

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(shopRoutes);
 
 // Catch all route
 app.use((req, res, next) => {
-  res.status(404).render('404', {docTitle: 'Pge Not Found'});
+  res.status(404).render('404', {docTitle: 'Page Not Found'});
   // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
--- routes/admin.js
+++ routes/admin.js
@@ -8,7 +8,7 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product',(req, res, next) => {
-  res.render('add-product', {docTitle: 'Add Product'})
+  res.render('add-product', {docTitle: 'Add Product', path:'/admin/add-product'})
   // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
