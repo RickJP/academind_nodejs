ca664ba176fd5376215fdca364fd90fcc1c4fbb7
Rick <3741326+RickJP@users.noreply.github.com>
2018-10-28 15:26:04 +0900

    Added product model and function to store product data in JSON file locally

--- app.js
+++ app.js
@@ -1,34 +1,24 @@
-const express = require('express');
-
-const adminData = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-const bodyParser = require('body-parser');
 const path = require('path');
-// const expressHbs = require('express-handlebars');
 
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
 
 const app = express();
 
-// app.engine(
-//   'hbs', 
-//   expressHbs({
-//     layoutsDir: 'views/layouts', 
-//     defaultLayout: 'main-layout',
-//     extname: 'hbs'
-//   }));
 app.set('view engine', 'ejs');
-//app.set('views', 'views');
+app.set('views', 'views');
 
-app.use(bodyParser.urlencoded({extended: false}));
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-// Catch all route
-app.use((req, res, next) => {
-  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
-  // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-});
+app.use(errorController.get404);
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -1,25 +1,11 @@
-const express = require('express');
 const path = require('path');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const express = require('express');
+
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/',(req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/', 
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-  // console.log('shop.js',adminData.products);
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-
-});
-
+router.get('/', productsController.getProducts);
 
 module.exports = router;
